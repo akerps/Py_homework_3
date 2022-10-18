f8898ca561d10b07713803ee4c8d7405f9e7da27
Анна <107682652+akerps@users.noreply.github.com>
2022-10-18 15:06:12 +0300

    Update 1_3.py
    исправлено

--- 1_3.py
+++ 1_3.py
@@ -5,8 +5,11 @@
 import random
 
 new_arr = [random.randint(0, 9) for i in range(random.randint(3, 12))]
-sum = 0
-for i in range(len(new_arr)):
-    if i % 2 != 0:
-        sum += new_arr[i]
-print(f'{new_arr} => сумма элементов на нечетных позициях равна {sum}')
+# sum = 0
+# for i in range(len(new_arr)):
+#     if i % 2 != 0:
+#         sum += new_arr[i]
+
+odd_list = [new_arr[i] for i in range(len(new_arr)) if i%2 != 0]
+
+print(f'{new_arr} => сумма элементов на нечетных позициях равна {sum(odd_list)}')
